c158508d8fb0e050bed354202054f908cbb6c396
Faheem Ahmad <fisforfaheem@gmail.com>
2021-07-31 16:34:50 +0500

    update

--- lib/const/commonColor.dart
+++ lib/const/commonColor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const grey = Color(0xff636363);
+const grey = Color(0xff636363); 
 const blue = Color(0xff7688d1);
 const newBlue = Color(0xff2a446b);
 const ptaNI = Color(0xffb5bee7);
